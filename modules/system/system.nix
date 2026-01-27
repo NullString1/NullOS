628b3e3fcf158c5e86c60bece9b1c132a75c1e3c
NullString1 <kernd2005@gmail.com>
2026-01-27 18:35:27 +0000

    Add logsmart cachix

--- modules/system/system.nix
+++ modules/system/system.nix
@@ -12,10 +12,12 @@
         "https://hyprland.cachix.org"
         "https://cache.nixos.org"
         "https://nix-community.cachix.org"
+        "https://logsmart-cache.cachix.org"
       ];
       trusted-public-keys = [
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "logsmart-cache.cachix.org-1:nhxeVYtlgc5IZ+6zALnIT/6PdZQHpjPwV+R0qwjm+BQ="
       ];
       trusted-users = [ vars.username ];
     };
