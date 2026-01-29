b38273c5e3e4f6d91a2519024084335ea7e5b4bc
NullString1 <kernd2005@gmail.com>
2026-01-29 16:25:32 +0000

    Add some packages to try fix lutris bugs

--- home/lutris.nix
+++ home/lutris.nix
@@ -1,4 +1,14 @@
 { pkgs, ... }:
 {
-  home.packages = [ pkgs.lutris ];
+  home.packages = [
+    (pkgs.lutris.override {
+      extraPkgs = pkgs: [
+        pkgs.vulkan-loader
+        pkgs.vulkan-tools
+        pkgs.vulkan-headers
+        pkgs.glib-networking
+        pkgs.gnutls
+      ];
+    })
+  ];
 }
--- modules/system/hardware_add.nix
+++ modules/system/hardware_add.nix
@@ -30,7 +30,24 @@ in
       extraBackends = [ pkgs.sane-airscan ];
       disabledDefaultBackends = [ "escl" ];
     };
-    graphics.enable = true;
+    graphics = {
+      enable = true;
+      enable32Bit = true;
+      extraPackages = with pkgs; [
+        intel-media-driver
+        intel-vaapi-driver
+        nvidia-vaapi-driver
+        vulkan-loader
+        gamemode
+      ];
+      extraPackages32 = with pkgs.pkgsi686Linux; [
+        vulkan-loader
+        gamemode
+        nvidia-vaapi-driver
+        intel-vaapi-driver
+        intel-media-driver
+      ];
+    };
     enableRedistributableFirmware = true;
     keyboard.qmk.enable = true;
     bluetooth.enable = true;
