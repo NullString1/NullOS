4bc0b6bcf1018ccf469b6c56ef7499426bae65c0
NullString1 <kernd2005@gmail.com>
2026-05-21 09:27:50 +0100

    Update nwg-displays for lua support

--- home/nwg-displays.nix
+++ home/nwg-displays.nix
@@ -1,6 +1,14 @@
 { pkgs, ... }:
 {
   home.packages = [
-    pkgs.nwg-displays
+    (pkgs.nwg-displays.overrideAttrs (old: {
+      version = "0.4.3";
+      src = pkgs.fetchFromGitHub {
+        owner = "nwg-piotr";
+        repo = "nwg-displays";
+        rev = "v0.4.3";
+        sha256 = "sha256-f7x6PTsND0eprhqvIdkZdHujcCbkJnqoXIKeE0O/YPE=";
+      };
+    }))
   ];
 }
