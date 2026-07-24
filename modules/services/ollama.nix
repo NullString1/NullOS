e481100eabd3ac0fec6347c6146c362eba6a9642
NullString1 <kernd2005@gmail.com>
2026-07-24 18:03:06 +0100

    Update model for ollama

--- modules/services/ollama.nix
+++ modules/services/ollama.nix
@@ -3,7 +3,7 @@
   services.ollama = {
     enable = true;
     openFirewall = true;
-    loadModels = [ "qwen3-instruct:4b" ];
+    loadModels = [ "huihui_ai/qwen3.5-abliterated:4b" ];
     host = if vars.enableExposeOllama then "[::]" else "127.0.0.1";
     package = if vars.useNvidia then pkgs.ollama-cuda else pkgs.ollama-cpu;
   };
