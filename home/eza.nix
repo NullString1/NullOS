0b8747a3eeb7f6c4755ca816fd33b4089ba9bb95
NullString1 <kernd2005@gmail.com>
2026-07-24 17:59:04 +0100

    Fix eza syntax

--- home/eza.nix
+++ home/eza.nix
@@ -12,7 +12,7 @@
       "--icons=always"
       # "--time-style=long-iso" # ISO 8601 extended format for time
       "--classify" # append indicator (/, *, =, @, |)
-      "--hyperlink" # make paths clickable in some terminals
+      "--hyperlink=auto" # make paths clickable in some terminals
     ];
   };
   home.shellAliases = {
